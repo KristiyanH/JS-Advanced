a755a92a68fe923633701418424206c2bafc7302
Kristiyan Hristov <71334312+KristiyanH@users.noreply.github.com>
2022-05-25 13:54:50 +0300

    +excersise(s)

---
+++ JS-Advanced-Labs/ArraysAndNestedArraysLab/4.NegativePositiveNumbers.js
@@ -0,0 +1,15 @@
+function solve(input) {
+    let result = [];
+
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] < 0) {
+            result.unshift(input[i]);
+        } else {
+            result.push(input[i]);
+        }
+    }
+
+    console.log(result);
+}
+
+solve([7, -2, 8, 9]);
