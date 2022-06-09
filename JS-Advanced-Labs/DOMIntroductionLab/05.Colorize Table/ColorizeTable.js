382e4c345a10b9855f7d665f3ed27e0b4a83b2fd
Kristiyan Hristov <71334312+KristiyanH@users.noreply.github.com>
2022-06-09 11:07:15 +0300

    +excersise(s)

--- JS-Advanced-Labs/DOMIntroductionLab/05.Colorize Table/ColorizeTable.js
+++ JS-Advanced-Labs/DOMIntroductionLab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,11 @@
 function colorize() {
-    
+    let rows = document.querySelectorAll('table tr');
+
+    let i = 0;
+    for (const row of rows) {
+        i++;
+        if (i % 2 == 0) {
+            row.style.backgroundColor = 'Teal'
+        }
+    }
 }
