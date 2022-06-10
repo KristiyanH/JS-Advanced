ca23099c51f9753f27776fd3a9b13bad14a7a29b
Kristiyan Hristov <71334312+KristiyanH@users.noreply.github.com>
2022-06-10 17:35:55 +0300

    +excersise(s)

--- JS-Advanced-Exercises/DOMIntroductionExercise/02. Pascal or Camel Case/solution.js
+++ JS-Advanced-Exercises/DOMIntroductionExercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,25 @@
-function solve() {
-  //TODO...
+function solve(text, casing) {
+    text = document.getElementById('text').value;
+    casing = document.getElementById('naming-convention').value;
+    let result = document.getElementById('result');
+
+    function camelCaseConvert(text) {
+        return text.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+    }
+
+    function pascalCaseConvert(text) {
+        return text.replace(/\w\S*/g, m => m.charAt(0).toUpperCase() + m.substr(1).toLowerCase()).replace(/\s+/g, '');
+    }
+
+    if (casing == 'Camel Case') {
+        let textResult = camelCaseConvert(text);
+        result.textContent = textResult;
+
+    } else if (casing == 'Pascal Case') {
+        let textResult = pascalCaseConvert(text);
+        result.textContent = textResult;
+    } else {
+        result.textContent = 'Error!';
+    }
+
 }
