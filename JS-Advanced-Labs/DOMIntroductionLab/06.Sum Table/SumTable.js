9ea7b66be04209312f04a41617a4dde9383c123a
Kristiyan Hristov <71334312+KristiyanH@users.noreply.github.com>
2022-06-10 16:26:29 +0300

    +excersise(s)

--- JS-Advanced-Labs/DOMIntroductionLab/06.Sum Table/SumTable.js
+++ JS-Advanced-Labs/DOMIntroductionLab/06.Sum Table/SumTable.js
@@ -1,4 +1,10 @@
 function sumTable() {
-    
+    let tds = document.querySelectorAll('td:nth-child(2n)');
+    let sum = 0;
+    let sumEl = document.getElementById('sum');
+    for (let i = 0; i < tds.length - 1; i++) {
+        sum += Number(tds[i].textContent);
+    }
 
+    sumEl.textContent = sum;
 }
--- JS-Advanced-Labs/DOMIntroductionLab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS-Advanced-Labs/DOMIntroductionLab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,14 @@
 function extract(content) {
+    const regex = new RegExp(/\(([^()]*)\)/g);
+    let text = document.getElementById(content).textContent;
+    let result = [];
 
+    let match = regex.exec(text);
+
+    while (match) {
+        result.push(match[1]);
+        match = regex.exec(text);
+    }
+
+    return result.join('; ');
 }
