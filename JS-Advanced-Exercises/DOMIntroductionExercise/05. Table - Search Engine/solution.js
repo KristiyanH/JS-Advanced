2a6d43a54e55f5e74441d92b72e6f2d84b451c27
Kristiyan Hristov <71334312+KristiyanH@users.noreply.github.com>
2022-06-11 14:31:50 +0300

    +excersise(s)

--- JS-Advanced-Exercises/DOMIntroductionExercise/05. Table - Search Engine/solution.js
+++ JS-Advanced-Exercises/DOMIntroductionExercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,14 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+    document.querySelector('#searchBtn').addEventListener('click', onClick);
+    let rows = document.querySelectorAll('tbody tr');
+    let searchText = document.getElementById('searchField');
 
-   function onClick() {
-      //   TODO:
-
-   }
+    function onClick() {
+        for (const row of rows) {
+            row.classList.remove('select');
+            if (row.innerHTML.includes(searchText.value)) {
+                row.className = 'select';
+            }
+        }
+    }
 }
--- JS-Advanced-Exercises/DOMIntroductionExercise/07. Hell's Kitchen/app.js
+++ JS-Advanced-Exercises/DOMIntroductionExercise/07. Hell's Kitchen/app.js
@@ -1,8 +1,61 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
-   }
+    function onClick() {
+        let input = JSON.parse(document.querySelector('#inputs textarea').value);
+        let avgSalary = 0;
+        let totalSalary = 0;
+        let currAverageSalary = 0;
+        let bestName = '';
+        let output = {};
+        //["Mikes - Steve 1000, Ivan 200, Paul 800", "Fleet - Maria 850, Janet 650"]
+
+        for (const inputElement of input) {
+            let restaurantInfo = inputElement.split(' - ');
+            let restaurantName = restaurantInfo.shift();
+            let workersData = restaurantInfo[0].split(', ');
+
+            for (const worker of workersData) {
+                let [name, salary] = worker.split(' ');
+
+                if (!output.hasOwnProperty(restaurantName)) {
+                    output[restaurantName] = {};
+                }
+                if (output.hasOwnProperty(restaurantName)) {
+                    output[restaurantName][name] = Number(salary);
+                }
+            }
+        }
+
+        let entries = Object.entries(output);
+
+        for (const entry of entries) {
+            let key = entry[0];
+            let values = Object.entries(entry[1]);
+
+            for (const [name, salary] of values) {
+                totalSalary += salary;
+            }
+
+            avgSalary = totalSalary / values.length;
+
+            if (avgSalary > currAverageSalary) {
+                currAverageSalary = avgSalary;
+                bestName = key;
+                totalSalary = 0;
+            }
+        }
+
+        let result = Object.entries(output[bestName])
+            .sort((a, b) => b[1] - a[1]);
+
+        let print = '';
+
+        result.forEach(x => print += `Name: ${x[0]} With Salary: ${x[1]} `);
+
+        document.querySelector('#bestRestaurant p')
+            .textContent = `Name: ${bestName} Average Salary: ${currAverageSalary.toFixed(2)} Best Salary: ${result[0][1].toFixed(2)}`;
+
+        document.querySelector('#workers p').textContent = print;
+    }
 }
