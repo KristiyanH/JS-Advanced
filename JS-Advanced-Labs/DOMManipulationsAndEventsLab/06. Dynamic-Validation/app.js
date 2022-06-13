3e5c31ff6e814c26b228f17e6800f4e43c18d2e4
Kristiyan Hristov <71334312+KristiyanH@users.noreply.github.com>
2022-06-13 17:35:22 +0300

    +excersise(s)

--- JS-Advanced-Labs/DOMManipulationsAndEventsLab/06. Dynamic-Validation/app.js
+++ JS-Advanced-Labs/DOMManipulationsAndEventsLab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    document.querySelector('input').addEventListener('change', onChange);
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    function onChange(ev) {
+        if (ev.target.value.match(regexEmail)) {
+            ev.target.className = '';
+            return;
+        }
+
+        ev.target.className = 'error';
+    }
 }
--- JS-Advanced-Labs/DOMManipulationsAndEventsLab/07. Shopping-Cart/solution.js
+++ JS-Advanced-Labs/DOMManipulationsAndEventsLab/07. Shopping-Cart/solution.js
@@ -1,3 +1,9 @@
 function solve() {
-   //TODO...
+    Array.from(document.querySelectorAll('button.add-product')).forEach(x => x.addEventListener('click', onClick));
+    let result = document.querySelector('textarea');
+    let products = document.querySelectorAll('.product');
+
+    function onClick(event) {
+        //TODO:
+    }
 }
