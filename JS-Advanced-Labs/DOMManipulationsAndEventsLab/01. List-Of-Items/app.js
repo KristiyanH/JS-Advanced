118c71bafd28af04aae1ae5eb1ccf9daf578e599
Kristiyan Hristov <71334312+KristiyanH@users.noreply.github.com>
2022-06-13 13:11:58 +0300

    +excersise(s)

--- JS-Advanced-Labs/DOMManipulationsAndEventsLab/01. List-Of-Items/app.js
+++ JS-Advanced-Labs/DOMManipulationsAndEventsLab/01. List-Of-Items/app.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+    let input = document.getElementById('newItemText').value;
+    let ul = document.querySelector('ul');
+
+    let newElement = document.createElement('li');
+    newElement.textContent = input;
+    ul.appendChild(newElement);
+
+    document.getElementById('newItemText').value = '';
 }
--- JS-Advanced-Labs/DOMManipulationsAndEventsLab/02. Delete-From-Table/app.js
+++ JS-Advanced-Labs/DOMManipulationsAndEventsLab/02. Delete-From-Table/app.js
@@ -1,3 +1,14 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let tbody = document.querySelector('tbody');
+    let input = document.querySelector('input[type="text"]').value;
+
+    let children = tbody.children;
+
+    for (const child of children) {
+        if (child.innerHTML.includes(input)) {
+            tbody.removeChild(child);
+        }
+    }
+
+    document.querySelector('input[type="text"]').value = '';
 }
