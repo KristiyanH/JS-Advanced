e02f6d9d07401f87b10852972d975b6508033e28
Kristiyan Hristov <71334312+KristiyanH@users.noreply.github.com>
2022-06-14 10:52:24 +0300

    +excersise(s)

--- JS-Advanced-Labs/DOMManipulationsAndEventsLab/07. Shopping-Cart/solution.js
+++ JS-Advanced-Labs/DOMManipulationsAndEventsLab/07. Shopping-Cart/solution.js
@@ -1,9 +1,26 @@
 function solve() {
-    Array.from(document.querySelectorAll('button.add-product')).forEach(x => x.addEventListener('click', onClick));
+    Array.from(document.querySelectorAll('button')).forEach(x => x.addEventListener('click', onClick));
     let result = document.querySelector('textarea');
-    let products = document.querySelectorAll('.product');
+    let cart = {};
 
     function onClick(event) {
-        //TODO:
+        if (event.target.className === 'add-product') {
+            let currentEl = event.target.parentElement;
+            let priceEl = Number(currentEl.nextElementSibling.innerHTML);
+            let productName = currentEl.previousElementSibling.children[0].innerHTML;
+
+            if (!cart[productName]) {
+                cart[productName] = 0;
+            }
+
+            cart[productName] += priceEl;
+
+            result.value += `Added ${productName} for ${priceEl.toFixed(2)} to the cart.\n`;
+        } else {
+            let totalPrice = Number(Object.values(cart).reduce((a, b) => a + b, 0));
+            result.value += `You bought ${Object.keys(cart).join(', ')} for ${totalPrice.toFixed(2)}.`;
+
+            Array.from(document.querySelectorAll('button')).forEach(x => x.disabled = true);
+        }
     }
 }
