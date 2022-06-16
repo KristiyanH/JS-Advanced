c3644e2fbf1cd860bfc8d5b4e321330ac21c0381
Kristiyan Hristov <71334312+KristiyanH@users.noreply.github.com>
2022-06-16 13:00:36 +0300

    +excersise(s)

--- JS-Advanced-Exercises/DOMManipulationsAndEventsExercise/06. Furniture/solution.js
+++ JS-Advanced-Exercises/DOMManipulationsAndEventsExercise/06. Furniture/solution.js
@@ -1,4 +1,31 @@
 function solve() {
+    let tBody = document.querySelector('tbody');
+    let genBtn = document.querySelector('#exercise button').addEventListener('click', getData);
+    let buyBtn = document.querySelector('#exercise > button:nth-child(6)').addEventListener('click', processData);
+    let inputArea = document.querySelector('#exercise > textarea:nth-child(2)');
+    let outputArea = document.querySelector('#exercise > textarea:nth-child(5)');
 
-  //TODO...
+    let boughtFurniture = [];
+    let totalPrice = 0;
+    let averageDecoration = 0;
+
+    function getData(event) {
+        let data = (JSON.parse(inputArea.value));
+
+        data.forEach(furniture => {
+            tBody.innerHTML += `<tr>
+          <td><img src=${furniture.img}></td>
+          <td><p>${furniture.name}</p></td>
+          <td><p>${furniture.price}</p></td>
+          <td><p>${furniture.decFactor}</p></td>
+          <td><input type="checkbox"/></td>
+          </tr>`
+        })
+
+    }
+
+    function processData(event) {
+        
+    }
 }
+
