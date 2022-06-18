a50eaecd9e41a6286eaba583c895f71edfd3d6b9
Kristiyan Hristov <71334312+KristiyanH@users.noreply.github.com>
2022-06-18 13:12:03 +0300

    +excersise(s)

--- JS-Advanced-Exercises/AdvancedFunctionsExercise/6.MonkeyPatcher.js
+++ JS-Advanced-Exercises/AdvancedFunctionsExercise/6.MonkeyPatcher.js
@@ -1,5 +1,24 @@
-function solve() {
+function solution(command) {
+    if (command === 'upvote') return this.upvotes++;
+    if (command === 'downvote') return this.downvotes++;
 
+    let upvote = this.upvotes;
+    let downvote = this.downvotes;
+    let totalVotes = upvote + downvote;
+    let balance = upvote - downvote;
+
+    function ratingState() {
+        if (totalVotes < 10) return 'new';
+        if (upvote > totalVotes * 0.66) return 'hot';
+        if (balance >= 0 && totalVotes > 100) return 'controversial';
+        if (balance < 0) return 'unpopular';
+        return 'new';
+    }
+
+    if (totalVotes > 50) {
+        let obfuscationValue = upvote > downvote ? Math.ceil(upvote * 0.25) : Math.ceil(downvote * 0.25);
+        return [upvote + obfuscationValue, downvote + obfuscationValue, balance, ratingState()];
+    }
+
+    return [upvote, downvote, balance, ratingState.call(this)];
 }
-
-solve();
